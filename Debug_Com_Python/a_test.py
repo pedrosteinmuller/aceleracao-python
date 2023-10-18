6571e8293eb4245fb016c4097e79878546ab8139
pedrosteinmuller <pedrosteinmuller10105@hotmail.com>
2023-10-18 20:09:13 -0300

    fixtures

--- Debug_Com_Python/a_test.py
+++ Debug_Com_Python/a_test.py
@@ -1,11 +1,31 @@
+import pytest
+
+
+@pytest.fixture  # Criamos a fixture por meio do decorador pytest.fixture
+def my_list():  # Por padrão, o nome da fixture será o nome da função
+    return [1, 2, 3]  # Retorna o valor que a fixture possuirá
+
+
 def test_a_simple_test():
     assert True
 
 
-def test_sum():
-    assert sum([1, 2, 3]) == 6
+def test_sum(my_list):  # Recebemos a fixture como parâmetro da função de teste
+    assert sum(my_list) == 6  # Usamos a lista retornada pela fixture
 
 
-def test_list_item_multiply():
-    my_list = [1, 2, 3]
+def test_list_item_multiply(my_list):  # Recebemos a mesma fixture aqui também
     assert [item * 3 for item in my_list] == [3, 6, 9]
+
+
+def test_print_to_stdout(capsys):
+    print("Hello, world!")
+    captured = capsys.readouterr()
+    assert captured.out == "Hello, world!\n"
+
+
+def test_error_to_stderr(capsys):
+    import sys
+    sys.stderr.write("Error message\n")
+    captured = capsys.readouterr()
+    assert captured.err == "Error message\n"
